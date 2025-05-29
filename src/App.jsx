0e3ed28b9fb6d0079e1d87ef2cdfa06724d03013
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-05-29 18:21:19 +0530

    setup: setup routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 function App() {
   return (
-    <div>App</div>
+    <Outlet />
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import WelcomePage from './pages/WelcomePage.jsx'
+import CreateAccount from './pages/CreateAccount.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import AccountSettings from './pages/AccountSettings.jsx'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index element={<WelcomePage/>}/>
+      <Route path='/create-account' element={<CreateAccount/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/account-settings' element={<AccountSettings/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
